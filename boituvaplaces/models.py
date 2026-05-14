63b558e17fb8b47caaac6f35a298f22d241ef298
marcosilveira2 <encivil.marco.silveira@gmail.com>
2026-05-14 18:00:05 -0300

    tabelas de paradas de onibos e rotas

--- boituvaplaces/models.py
+++ boituvaplaces/models.py
@@ -13,8 +13,22 @@ class Rotas(models.Model):
 class ParadasOnibus(models.Model):
     endereco = models.CharField(max_length=200)
     rotas = models.ManyToManyField(Rotas, related_name='pontos')
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.endereco
+
+
+class CategoriaMarco(models.Model):
+    # Ex: "Visual", "Turístico", "Funcional"
+    nome = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+
+
+
 
     
