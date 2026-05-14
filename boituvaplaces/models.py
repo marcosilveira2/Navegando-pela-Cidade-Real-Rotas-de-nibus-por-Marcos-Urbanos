d27462f016cb8548a6d7c7873ea69595058fa122
marcosilveira2 <encivil.marco.silveira@gmail.com>
2026-05-14 17:47:29 -0300

    tabelas de paradas de onibos e rotas

--- boituvaplaces/models.py
+++ boituvaplaces/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Models que definem o banco de dados
+
+class Rotas(models.Model):
+    codigo = models.CharField(max_length=10, unique=True) 
+    nome  = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nome}"    
+
+
+class ParadasOnibus(models.Model):
+    endereco = models.CharField(max_length=200)
+    rotas = models.ManyToManyField(Rotas, related_name='pontos')
+
+    def __str__(self):
+        return self.endereco
+
+    
